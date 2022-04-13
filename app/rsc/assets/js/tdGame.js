8a6c70126505ff3b03c9c055f4e35a15dee43a4e
Eric <contact@sanma.fr>
2022-04-14 00:47:27 +0200

    refactor: game logic functions

--- app/rsc/assets/js/tdGame.js
+++ app/rsc/assets/js/tdGame.js
@@ -2,8 +2,9 @@ class TDGame {
     constructor(triWeight, cirWeight, squWeight, croWeight, targetMin, targetMax,
                 timeLearning, nbSliders, nbLocks,
                 shapeNames = ["Triangle", "Circle", "Square", "Cross"],
-                showTimeline = true) {
+                showTimeline = true, easyMode = false) {
         this.totalClicks = 0
+        this.weights = [triWeight, cirWeight, squWeight, croWeight]
         this.triWeight = triWeight
         this.cirWeight = cirWeight
         this.squWeight = squWeight
@@ -17,8 +18,14 @@ class TDGame {
         this.nbLocks = nbLocks
         this.timeLearning = timeLearning
         this.currStep = 0
+        this.easyMode = easyMode
 
+        this.currShape = ""
         this.currShapeGrid = []
+    }
+
+    initFirstStep(){
+        this.currShape = this.pickNewShape()
     }
 
     addClick(){
@@ -29,7 +36,16 @@ class TDGame {
         return this.triWeight + this.cirWeight + this.squWeight + this.croWeight
     }
 
-
+    pickNewShape(){
+        let rand = Math.floor(Math.random() * this.sumWeight())
+        let s = 0
+        for(let i = 0; this.shapeNames.length; i++) {
+            s += this.weights[i]
+            if(rand < s)
+                return this.shapeNames[i]
+        }
+        return this.shapeNames[this.shapeNames.length - 1]
+    }
 
     async endGamePOST(ipAdress, NB_LOCKS){
         const options = {
